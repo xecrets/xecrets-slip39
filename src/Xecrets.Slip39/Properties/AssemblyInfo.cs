599e72017b32b95d3aff678ba18d33a717a0265b
Svante Seleborg <svante@axantum.com>
2024-10-30 11:39:36 +0100

    :construction_worker: Prepare for CI nuget package build etc #3

--- src/Xecrets.Slip39/Properties/AssemblyInfo.cs
+++ src/Xecrets.Slip39/Properties/AssemblyInfo.cs
@@ -1,11 +1,48 @@
+#region Copyright and MIT License
+/* MIT License
+ *
+ * Copyright © 2024 Svante Seleborg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+#endregion Copyright and MIT License
+
 using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// In SDK-style projects such as this one, several assembly attributes that were historically
-// defined in this file are now automatically added during build and populated with
-// values defined in project properties. For details of which attributes are included
-// and how to customise this process see: https://aka.ms/assembly-info-properties
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xecrets Slip39 BETA Shamir's Secret-Sharing")]
+[assembly: AssemblyDescription("Shamir's Secret-Sharing for Mnemonic Codes in C#")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Axantum Software AB")]
+[assembly: AssemblyProduct("Xecrets Slip39")]
+[assembly: AssemblyCopyright("Copyright © 2022-2024 Lucas Ontivero, Svante Seleborg, All Rights Reserved")]
+[assembly: AssemblyTrademark("Xecrets is a trademark of Axantum Software AB")]
+[assembly: AssemblyCulture("")]
 
 
 // Setting ComVisible to false makes the types in this assembly not visible to COM
@@ -17,5 +54,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM.
 
 [assembly: Guid("3c0adc00-14e7-46c2-a91b-0cc99fe04725")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("Xecrets.Slip39.Test")]
