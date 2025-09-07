9a7b675058013b1cec2438c4f0e0f22cb14a51b8
Svante Seleborg <svante@axantum.com>
2025-09-07 10:10:24 +0200

    :bookmark: Add missing AssemblyInformationalVersion #14

--- src/Xecrets.Slip39/Properties/AssemblyInfo.cs
+++ src/Xecrets.Slip39/Properties/AssemblyInfo.cs
@@ -48,6 +48,7 @@ using System.Resources;
 
 [assembly: AssemblyVersion("2.3.0.0")]
 [assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyInformationalVersion("2.3.0.0")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
